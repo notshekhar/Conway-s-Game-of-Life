dc9f7c313ac9294e064c864dc320684b10218f56
Shekhar Tyagi <36845555+notshekhar@users.noreply.github.com>
2020-09-27 08:24:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
 const [canvasWidth, canvasHeight] = [canvas.width, canvas.height]
-const res = 5
+const res = 10
 const [rows, cols] = [canvasWidth / res, canvasHeight / res]
 
 function createGrid(rows, cols) {
